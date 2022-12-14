d16808392a88ae7d11f30c14e7fa9b25c5922ffe
shanuka27 <shanukayasiru27@gmail.com>
2022-12-14 13:44:57 +0530

    lesson 1

---
+++ L1/L1.js
@@ -0,0 +1,39 @@
+var userOne = {
+    email: 'shanu@gmail.com',
+    name: 'shanu',
+
+    login(){
+        console.log(this.email, 'has logged in')
+    },
+
+    logout(){
+        console.log(this.email, 'has logged out')
+    }
+};
+
+var userTwo = {
+    email: 'yasiru@gmail.com',
+    name: 'yasiru',
+
+    login(){
+        console.log(this.email, 'has logged in')
+    },
+
+    logout(){
+        console.log(this.email, 'has logged out')
+    }
+};
+
+var userThree = {
+    email: 'gamage@gmail.com',
+    name: 'gamage',
+
+    login(){
+        console.log(this.email, 'has logged in')
+    },
+
+    logout(){
+        console.log(this.email, 'has logged out')
+    }
+};
+
